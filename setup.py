b4e935a7f8139e9657e64a63af30d41115538ab1
Psycoguana <21992381+Psycoguana@users.noreply.github.com>
2020-07-23 20:23:27 -0300

    Added dependencies for PyPi

--- setup.py
+++ setup.py
@@ -13,11 +13,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Psycoguana/Filter-for-Reddit",
     packages=setuptools.find_packages(),
-    scripts = ['bin/ffr'],
+    install_requires=[
+        'praw',
+        'rich',
+        'click'
+    ],
+    scripts=['bin/ffr'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.6'
 )
