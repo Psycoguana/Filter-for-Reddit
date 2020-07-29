412786a7eeff0840ce4bcb1a98a3806a77a80a5e
Psycoguana <21992381+Psycoguana@users.noreply.github.com>
2020-07-29 19:06:02 -0300

    Added check so the directory can be changed safely from an exe

--- ffr/main.py
+++ ffr/main.py
@@ -119,7 +119,13 @@ def login():
 def main():
     # This sets the Current Working Directory to wherever the script is located.
     # Ensuring the script gets the correct path of praw.ini no matter from where it's called.
-    script_dir = pathlib.Path(__file__).parent.absolute()
+    # To do it, it's important to check if we're running from an exe or not.
+    
+    if getattr(sys, 'frozen', False): # Running from an exe
+        script_dir = pathlib.Path(sys.executable).parent.absolute().parent.absolute()
+    elif __file__:
+        script_dir = pathlib.Path(__file__).parent.absolute()
+    
     os.chdir(script_dir)
 
     if len(sys.argv) == 1:
