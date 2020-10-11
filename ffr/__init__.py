a6f4a15bd6622a32e4d13d2396f98e8c678ac972
Psycoguana <21992381+Psycoguana@users.noreply.github.com>
2020-10-11 19:26:27 -0300

    Organized some code

--- ffr/__init__.py
+++ ffr/__init__.py
@@ -1,3 +1,18 @@
 from pkg_resources import get_distribution
+import pathlib
+import sys
+import os
 
 __version__ = get_distribution('ffr').version
+
+# This sets the Current Working Directory to wherever the script is located.
+# Ensuring the script gets the correct path of praw.ini no matter from where it's called.
+# To do it, it's important to check if we're running from an exe or not.
+
+if getattr(sys, 'frozen', False):  # Running from an exe
+    script_dir = pathlib.Path(
+        sys.executable).parent.absolute().parent.absolute()
+elif __file__:
+    script_dir = pathlib.Path(__file__).parent.absolute()
+
+os.chdir(script_dir)
