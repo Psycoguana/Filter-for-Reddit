6efd71eb18a36c2627ce896238d6cd9e0d3c9a10
Psycoguana <21992381+Psycoguana@users.noreply.github.com>
2020-11-04 15:15:34 -0300

    Bumped version to 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ffr",
-    version="0.0.3",
+    version="0.0.4",
     author="Psycoguana",
     author_email="",
     description="Filter for Reddit saves",
